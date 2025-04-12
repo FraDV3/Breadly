3ca12b0b4c55f1edbc2a157775dc3c1fa4d9f480
Francesco De Vivo <francescodvf@yahoo.it>
2025-04-12 19:17:57 +0200

    Added main index.html and index.js for React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Breadly 🍞</h1>
+      <p>Start baking your app here.</p>
+    </div>
+  );
+}
+
+export default App;
